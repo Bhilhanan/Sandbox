07b3edbe1b242ccd58620cd209673adde2e3b557
Bhilhanan Jeyaram <bhilhanan_jeyaram@gap.com>
2016-06-21 12:44:05 -0700

    Adding 3 numbers to get a sum

--- src/ElementsAddToGivenSum.java
+++ src/ElementsAddToGivenSum.java
@@ -10,26 +10,51 @@ import org.junit.Test;
 public class ElementsAddToGivenSum {
 
 	@Test
-	public void shouldFindElementsThatAddToGivenSum() {
-		int[] input = { 1, 4, 3, 2,3,2, 1 - 3, 2, -8, 9, 2 ,-4,5,0};
-		List<Integer[]> result = findElementsWithSum(input, 5);
-		for(Integer[] i:result){
-			System.out.println(i[0]+" "+i[1]);
+	public void shouldFind2ElementsThatAddToGivenSum() {
+		int[] input = { 1, 4, 3, 2, 3, 2, 1 - 3, 2, -8, 9, 2, -4, 5, 0 };
+		Map<Integer, Integer> result = find2ElementsWithSum(input, 5);
+		for (Entry<Integer, Integer> i : result.entrySet()) {
+			System.out.println(i.getKey() + " " + i.getValue());
 		}
 	}
 
-	private List<Integer[]> findElementsWithSum(int[] input, int sum) {
+	@Test
+	public void shouldFind3ElementsThatAddToGivenSum() {
+		int[] input = { 1, 4, 3, 2, 3, 2, 1 - 3, 2, -8, 9, 2, -4, 5, 0 };
+		Map<Integer, Integer[]> result = find3ElementsWithSum(input, 5);
+		for (Entry<Integer, Integer[]> entry : result.entrySet()) {
+			System.out.println(entry.getKey() + " " + entry.getValue()[0]+" "+entry.getValue()[1]);
+		}
+	}
+
+	private Map<Integer, Integer[]> find3ElementsWithSum(int[] input, int sum) {
+		Map<Integer,Integer[]> result=new HashMap<>();
+		for(int i=0;i<input.length;i++){
+			int tmpSum=sum-input[i];
+			Map<Integer, Integer> tmpResult = find2ElementsWithSum(input, tmpSum);
+			if(tmpResult.isEmpty()){
+				continue;
+			}
+			for(Entry<Integer, Integer> a:tmpResult.entrySet()){
+				Integer[] array={a.getKey(),a.getValue()};
+				result.put(input[i], array);
+			}
+		}
+		return result;
+	}
+
+	private Map<Integer, Integer> find2ElementsWithSum(int[] input, int sum) {
 		int start = 0;
 		int end = input.length - 1;
 		List<Integer[]> result = new ArrayList<>();
-		Map<Integer,Integer> resultMap=new HashMap<>();
+		Map<Integer, Integer> resultMap = new HashMap<>();
 		Arrays.sort(input);
 		while (start < end) {
 			int actualSum = input[start] + input[end];
 			if (actualSum == sum) {
 				Integer[] a = { Integer.valueOf(input[start]), Integer.valueOf(input[end]) };
 				result.add(a);
-				resultMap.put(a[0],a[1]);
+				resultMap.put(a[0], a[1]);
 				start++;
 				end--;
 				continue;
@@ -40,11 +65,11 @@ public class ElementsAddToGivenSum {
 				end--;
 			}
 		}
-		for(Entry<Integer, Integer> a:resultMap.entrySet()){
-			System.out.println(a.getKey()+" "+a.getValue());
-		}
-		System.out.println("");
-		return result;
+//		for (Entry<Integer, Integer> a : resultMap.entrySet()) {
+//			System.out.println(a.getKey() + " " + a.getValue());
+//		}
+//		System.out.println("");
+		return resultMap;
 	}
 
 }
